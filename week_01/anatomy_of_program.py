065a1c9025c5b4460fc7744d59ae27a3d1ac3f70
Surajit Bose <verbose@duck.com>
2025-02-03 23:06:56 -0800

    Add program skeleton

---
+++ week_01/anatomy_of_program.py
@@ -0,0 +1,26 @@
+"""
+main.py
+
+Karel moves a beeper from the bottom row to the top row
+
+Programmer: Surajit A Bose, Date: 2025.01.06
+"""
+
+from karel.stanfordkarel import *
+
+def main():
+    """Move Karel and beeper to top row
+    
+    Preconditions:
+    - Karel is in the bottom left corner, facing east
+    - There is a beeper immediately in front of Karel
+
+    Postconditions:
+    - Karel is in the top right corner, facing east
+    - Beeper has been moved and is immediately behind Karel
+    """
+    pass   # Delete this line and write your code here!  :)
+
+# There is no need to edit code beyond this point
+if __name__ == '__main__':
+    main()
