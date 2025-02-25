b22dfef2fea2e62b277c5c6cc4ab18cd54e6e764
Surajit Bose <verbose@duck.com>
2025-02-24 18:55:51 -0800

    Add template for code with params and returns

---
+++ week_08/template_param_return.py
@@ -0,0 +1,41 @@
+"""
+filename.py
+
+Description of problem
+
+Programmer: Your name here, Date: date modified here
+"""
+
+# import some_library statements
+# from some_library import some_code statements
+
+# CONSTANTS
+
+def main():
+    """
+    Brief overview of what main() does
+    
+    Preconditions:
+    
+    Postconditions:
+    """
+    # write out high-level steps here
+
+
+
+# Helper functions, as many as needed to implement the high-level steps
+def … ():
+    """
+    Brief description of what this function does
+    
+    Parameters:
+    
+    Preconditions:
+    
+    Postconditions:
+    
+    Returns: 
+    """
+
+if __name__ == "__main__":
+    main()
