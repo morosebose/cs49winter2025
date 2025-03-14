41373062151c7cdab0a1b925e1c79c18515e77a6
Surajit Bose <verbose@duck.com>
2025-03-14 14:30:52 -0700

    Correct error in section slides. Revise list_parameter.py

--- week_10/list_parameter.py
+++ week_10/list_parameter.py
@@ -1,28 +1,46 @@
 """
 list_parameter.py
 
-Demonstrate how passing a list as a parameter works
+Demonstrate difference between passing ints and lists as parameters:
 
-Programmer: Surajit A. Bose, Date: 2024.06.14
+Programmer: Surajit A. Bose, Date: 2025.03.14
 """
 
 def add_two_elements(some_list):
     """Accept a list as an input argument and add two elements at the end"""
     some_list.extend([6, 7])
+    print(f'List in add_two_elements() is: {some_list}')
+    
+    
+def add_two_to_number(num):
+    num += 2
+    print(f'Number in add_two_to_number() is: {num}')
     
 
 def main():
-    """Create a list with three elements. Use it as argument to a mutating function."""
-    # create and print original list
+
+    # assign an integer to a variable
+    my_num = 2
+    # create original list
     my_list = [1, 2, 3]
-    print(f'Original list: {my_list}')
     
-    # call mutating function
-    print('Passing list as argument to mutating function')
+    #print original values
+    print('Before function calls:')
+    print(f'Number in main() is: {my_num}')
+    print(f'List in main() is: {my_list}')
+    print()
+    
+    # call functions
+    print('As functions execute:')
+    add_two_to_number(my_num)
     add_two_elements(my_list)
+    print()
     
-    # print list after call to mutating function
-    print(f'Resultant list: {my_list}')
-    
+    # print values in main list after functions are done executing
+    print('After functions are done executing:')
+    print(f'Number in main() is: {my_num}')
+    print(f'List in main() is: {my_list}')
+    print()
+        
 if __name__ == "__main__":
     main()
