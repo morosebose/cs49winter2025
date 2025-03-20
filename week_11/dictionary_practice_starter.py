fec44cf210ddb8f71118aeb598cda2a1ffe72660
Surajit Bose <verbose@duck.com>
2025-03-19 23:36:46 -0700

    Add week 11 slides and starter code

---
+++ week_11/dictionary_practice_starter.py
@@ -0,0 +1,39 @@
+def main():
+    
+    # I have three dogs. Rover is a lab, Renly a mutt, and Spot a pug. 
+    # Create a new dictionary breeds with the names as keys, the breeds as values.
+
+
+    # Fetch Renly's breed and store it in a variable called renly_breed
+
+
+    # I have a new dog! Her name is Fifi. 
+    # She is also a mutt. Add Fifi to the breeds dictionary. 
+
+
+    # Oops! Fifi is very upset. She says she is not a mutt, she is a poodle!
+    # Correct Fifi's breed in the dictionary at once.
+
+
+    # H'm I can't remember whether my old chihuahua Max is in the dictionary.
+    # Can you check? Print "Yes, Max is here!" or "Nope, no Max." as appropriate.
+    print("Is Max in the dictionary?")
+    
+    
+    
+    print()
+    # Is any of my dogs a pit bull? Print "Yes, {dog_name} is a pit bull"
+    # or "Nope, no pits here. Pity." as appropriate. 
+    print("Are there any pit bulls in the dictionary?")
+    
+    
+    
+    print()
+    # Print all the dogs and their breeds, one line at a time.
+    # The format should be (f"{dog} is a {breed}")
+
+
+
+
+if __name__ == "__main__":
+    main()
